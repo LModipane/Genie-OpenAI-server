cc2e01e43d10a96addd0ced9ac383d5e45779079
shaun <modipanesh@gmail.com>
2023-03-21 12:24:19 +0200

    fixed error

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ app.get('/', async (req, res) => {
 
 app.post('/', async (req, res) => {
 	try {
-		const { prompt } = res.body;
+		const prompt = req.body.prompt;
 		const responce = await openai.createCompletion({
 			model: 'text-davinci-003',
 			prompt: `${prompt}`,
